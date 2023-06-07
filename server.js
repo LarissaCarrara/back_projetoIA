6c37f25515b0cbe3f547d866843f997168f58d99
Lucas A. Camacho <111870284+LcsCamacho@users.noreply.github.com>
2023-06-07 20:40:16 -0300

    fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors"); //segurança do back
 const iaRouters = require("./ia_route");
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.use(iaRouters);
 
-app.listen(3001, ()=>{
-    console.log("rodando");
-})
+app.listen(3001, () => {
+  console.log("rodando");
+});
